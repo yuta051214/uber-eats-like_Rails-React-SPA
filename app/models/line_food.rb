d47b6855dc88a1fc5096876d3ab990ac17513c9a
yuta051214 <ffftrick123@gmail.com>
2023-02-18 11:28:33 +0900

    Create Controller

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -1,10 +1,11 @@
-class lineFood < ApplicationRecord
+class LineFood < ApplicationRecord
   belongs_to :food
   belongs_to :restaurant
   # optional: true は、外部キーのnilを許可する(関連付けを任意にする)
   belongs_to :order, optional: true
 
   validates :count, numericality: { greater_than: 0 }
+
   # 下記のscopeを設定することにより、LineFood.active とすることで、全てのLineFoodからwhereでactive: trueなものの一覧をActiveRecord_Relationオブジェクトとして返す。
   scope :active, -> { where(active: true ) }
   # restaurant_id が特定の店舗IDでないものの一覧を返す。他店舗のLineFoodの有無の確認に用いる。
